df842254f392d525fe88aae11785020682b23012
Trung Dang <trungdt@absoft.vn>
2016-03-31 08:28:42 +0700

    Add database

---
+++ core/database.js
@@ -0,0 +1,1 @@
+// init sequelize connection here
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // https://github.com/m1x0n/express-sequelize-postgres-todo
 
+require("./config");
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,7 +9,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

b2528d30c0272e70e41bdac212e9d68e96c5f0c5
Trung Dang <trungdt@absoft.vn>
2016-04-17 22:30:03 +0700

    fdsa

--- amqp/client.js
+++ amqp/client.js
@@ -4,17 +4,19 @@ var connection = amqp.createConnection({
 });
 var count = 1;
 
-connection.on('ready', function() {
-    var sendMessage = function(connection, queue_name, payload) {
+connection.on('ready', function () {
+    var sendMessage = function (connection, queue_name, payload) {
         var encoded_payload = JSON.stringify(payload);
         connection.publish(queue_name, encoded_payload);
     }
 
-    for (var i = 0; i < 100; i++) {
+    var i = 0;
+    setInterval(function () {
+        i++;
         var payload = {
             zed: i,
             t1: Date.now()
         };
         sendMessage(connection, 'my-queue', payload);
-    }
+    }, 100);
 })
--- amqp/server.js
+++ amqp/server.js
@@ -1,21 +1,28 @@
 var amqp = require('amqp');
 
-var connection = amqp.createConnection({ 
-    host: '192.241.179.185' 
+var connection = amqp.createConnection({
+    host: '192.241.179.185'
 });
 
 // Wait for connection to become established.
 connection.on('ready', function () {
     console.log("amqp ready");
-  // Use the default 'amq.topic' exchange
-  connection.queue('my-queue', function (q) {
-      // Catch all messages
-      q.bind('#');
+    // Use the default 'amq.topic' exchange
+    connection.queue('my-queue', function (q) {
+        // Catch all messages
+        q.bind('#');
 
-      // Receive messages
-      q.subscribe(function (message) {
-        // Print messages to stdout
-        console.log(message);
-      });
-  });
+        // Receive messages
+        q.subscribe(function (message) {
+            // Print messages to stdout
+            var buffer = new Buffer(message.data);
+            var msg = buffer.toString();
+            var payload = JSON.parse(msg);
+
+            var when = Date.now();
+            var dt = when - payload.t1;
+            console.log("with i = " + payload.zed + "; delta t = " + dt);
+            // done(null, { t1: args.t1, zed: args.zed, bar: args.zed + 1, when: when })
+        });
+    });
 });

67452567d6bbfb3442100c7f99714cba50e94d86
Trung Dang <trungdt@absoft.vn>
2016-04-17 22:33:16 +0700

    fdsa

--- amqp/client2.js
+++ amqp/client2.js
@@ -5,14 +5,31 @@ var seneca = require('seneca')()
         url: 'amqp://guest:guest@192.241.179.185:5672',
         pin: 'role:create'
     });
-    
 
-for (var i = 0; i < 100; i++) {
+
+// for (var i = 0; i < 100; i++) {
+//     var t1 = Date.now();
+//     seneca.act('role:create,foo:1,zed:' + i + ",t1:" + t1, function (err, ret) {
+//         var when = Date.now();
+//         var dt = when - ret.when;
+//         var dt2 = when - ret.t1;
+//         console.log("with i = " + ret.zed + "; dt1 = " + dt + "; dt2 = " + dt2 + " ;  t1 = " + ret.when + " ; t2= " + when);
+//     });
+// }
+
+var i = 0;
+setInterval(function () {
+    i++;
     var t1 = Date.now();
-    seneca.act('role:create,foo:1,zed:' + i + ",t1:" + t1, function(err, ret) {
+    var payload = {
+        zed: i,
+        t1: t1
+    };
+    // sendMessage(connection, 'my-queue', payload);
+    seneca.act('role:create,foo:1,zed:' + i + ",t1:" + t1, function (err, ret) {
         var when = Date.now();
         var dt = when - ret.when;
         var dt2 = when - ret.t1;
-        console.log("with i = " + ret.zed + "; dt1 = " + dt + "; dt2 = " + dt2 + " ;  t1 = " + ret.when + " ; t2= " + when);
+        console.log("with i = " + ret.zed + "; dt1 = " + dt + "; dt2 = " + dt2 );
     });
-}
+}, 100);
